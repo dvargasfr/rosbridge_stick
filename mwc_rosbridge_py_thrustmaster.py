7c78469b096dba3510ebf367275e560f2f7a09d2
david <dvargasfr@gmail.com>
2019-02-01 19:24:03 +0100

    multithreading

--- mwc_rosbridge_py_thrustmaster.py
+++ mwc_rosbridge_py_thrustmaster.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 from __future__ import print_function
+from threading import Thread
 import roslibpy
 import inputs
+import time
 
 print(inputs.devices.gamepads)
 
@@ -10,6 +12,7 @@ BTN_TRIGGER = 0
 ABS_X = 0
 ABS_Y = 0
 ABS_THROTTLE = 1
+GO_FORWARD = 1
 
 # Offset for XY axis values boundaries (-1 to 1)
 OFFSET_XY = -128.0
@@ -23,42 +26,78 @@ OFFSET_THROTTLE = 0.0
 # Coeficient to set THROTTLE axis values between 0 and 1
 COEF_THROTTLE = 255.0
 
-
+freq_cnt = 0
 
 #ros = roslibpy.Ros(host='192.168.12.20', port=9090)
 ros = roslibpy.Ros(host='localhost', port=9090)
 ros.run()
 ros.on_ready(lambda: print('Is ROS connected?', ros.is_connected))
 talker = roslibpy.Topic(ros, '/cmd_vel', 'geometry_msgs/Twist')
-try:
+
+def get_gamepad_events():
+    global BTN_TRIGGER
+    global ABS_X
+    global ABS_Y
+    global OFFSET_XY
+    global COEF_XY
+    global GO_FORWARD
+    global ABS_THROTTLE
+    global COEF_THROTTLE
+    global OFFSET_THROTTLE
     while True:
         events = inputs.get_gamepad()
         for event in events:
             if event.code == 'BTN_TRIGGER':
                 BTN_TRIGGER = event.state;
             if event.code == 'ABS_X':
-                ABS_X = (event.state + OFFSET_XY) / COEF_XY;
+                ABS_X = (event.state + OFFSET_XY) / COEF_XY * GO_FORWARD;
             if event.code == 'ABS_Y':
                 ABS_Y = (event.state + OFFSET_XY) / COEF_XY;
+                if(ABS_Y < -0.05):
+                    GO_FORWARD = -1;
+                else:
+                    GO_FORWARD = 1;
             if event.code == 'ABS_THROTTLE':
-                ABS_THROTTLE = ((COEF_THROTTLE - event.state) / COEF_THROTTLE) + OFFSET_THROTTLE;
-                #ABS_THROTTLE = event.state;
-            if BTN_TRIGGER == 1:
-                print('Trigger ', BTN_TRIGGER)
-                print('X ', ABS_X)
-                print('Y ', ABS_Y)
-                print('Throttle ',ABS_THROTTLE)
-                talker.publish(roslibpy.Message({
-                    'linear': {
-                        'x':ABS_Y*ABS_THROTTLE,
-                        'y':0.0,
-                        'z':0.0},
-                    'angular':{
-                        'x':ABS_THROTTLE,
-                        'y':0.0,
-                        'z':ABS_X*ABS_THROTTLE
-                    }}))
+                ABS_THROTTLE = (( CEF_THROTTLE - event.state) / COEF_THROTTLE) + OFFSET_THROTTLE;
+
+
+try:
+    thread = Thread(target = get_gamepad_events, args = ())
+    thread.start()
+    while True:
+        time.sleep(0.1)
+        if BTN_TRIGGER == 1:
+            print('Trigger ', BTN_TRIGGER)
+            print('X ', ABS_X)
+            print('Y ', ABS_Y)
+            print('Throttle ',ABS_THROTTLE)
+            talker.publish(roslibpy.Message({
+                'linear': {
+                    'x':ABS_Y*ABS_THROTTLE,
+                    'y':0.0,
+                    'z':0.0},
+                'angular':{
+                    'x':0.0,
+                    'y':0.0,
+                    'z':ABS_X*ABS_THROTTLE
+                }}))
+        else:
+            print('Trigger ', BTN_TRIGGER)
+            print('X 0.0')
+            print('Y 0.0')
+            print('Throttle ',ABS_THROTTLE)
+            talker.publish(roslibpy.Message({
+                'linear': {
+                    'x':0.0,
+                    'y':0.0,
+                    'z':0.0},
+                'angular':{
+                    'x':0.0,
+                    'y':0.0,
+                    'z':0.0
+                }}))
 except KeyboardInterrupt:
-    pass
+    thread.stop()
+    cleanup_stop_thread()
 
 ros.terminate()
---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+freq_cnt = 0
+for i in range(10):
+    print ('i: ',i)
+    print ('i%2: ',i%2)
